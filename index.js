120889f6c815f6dc2164a6b98d4d50042c639eb4
João Diego <jdflorencio@gmail.com>
2019-10-27 05:43:36 -0300

    autenticação JWT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 // require('dotenv').config();
-// var jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const routes = require('./app/Modules/Cliente/Routes/ClienteRoute');
 const routesUser = require('./app/Modules/User/Routes/UserRoute');
