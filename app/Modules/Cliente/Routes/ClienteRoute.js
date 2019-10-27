18a96c608fcf202a3214715719bb510d5ad4c156
João Diego <jdflorencio@gmail.com>
2019-10-27 05:44:08 -0300

    refatorando

--- app/Modules/Cliente/Routes/ClienteRoute.js
+++ app/Modules/Cliente/Routes/ClienteRoute.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
+const verify = require('../../../Middlewares/auth_jwt')
 
-const clienteController = require('../Controllers/ClienteController');
+const clienteController = require('../Controllers/ClienteController')
+routes.use(verify)
 
-routes.get('/', clienteController.index);
-routes.get('/:idCliente', clienteController.cliente);
-routes.post('/' ,clienteController.create);
-routes.put('/', clienteController.update);
-routes.delete('/:idCliente', clienteController.delete);
+routes.get('/' ,clienteController.index)
+routes.get('/:idCliente', clienteController.cliente)
+routes.post('/' ,clienteController.create)
+routes.put('/', clienteController.update)
+routes.delete('/:idCliente', clienteController.delete)
 
-module.exports = routes;
+module.exports = routes
--- app/Modules/User/Routes/UserRoute.js
+++ app/Modules/User/Routes/UserRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
+
 const userController = require('../Controllers/UserController');
 
 routes.get('/login', userController.login);
