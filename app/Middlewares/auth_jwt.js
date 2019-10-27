42001e9fb6fad7e1cd7f81f033add5bce79067f7
João Diego <jdflorencio@gmail.com>
2019-10-27 05:40:05 -0300

    função reponsavel por validar a autenticação

--- app/Middlewares/auth_jwt.js
+++ app/Middlewares/auth_jwt.js
@@ -1,14 +1,15 @@
+
+var jwt = require('jsonwebtoken');
 const verifyJWT = (req, res, next) => {
-    var token = req.headers['x-access-token']
 
-    if (!token) return res.status(401).send({auth: false, message: "No token provided."})
-    
-    jwt.very(token, process.env.SECRET, function(err, decoded) {
-        if (err) return  res.satuts(500).send({ auth: false, message: "Failed to authenticate token."})
+	var token = req.headers['x-access-token']
+	if (!token) return res.status(401).send({auth: false, message: "No token provided."})	
 
-        req.userId = decoded.id
-        next()
-
-    })
-
+	jwt.verify(token, process.env.SECRET, function(err, decoded) {
+			if (err) return  res.satuts(500).send({ auth: false, message: "Failed to authenticate token."})
+			req.userId = decoded.id
+			next()
+	})
 }
+
+module.exports= verifyJWT
