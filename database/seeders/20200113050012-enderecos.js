e39c4a0022f36f50d8897eea49ee1612a86e8c31
João Diego <jdflorencio@gmail.com>
2020-01-13 02:06:06 -0300

    adicionando o seed enderecos

---
+++ database/seeders/20200113050012-enderecos.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkinsert('enderecos', [
+      {
+        clientesId: 1,
+        endereco : "Travessa Francisco Alves",
+        bairro :  "Marechal Rondon",
+        numero :  555,
+        complemento : "perto da mercado da esquina",
+        cidade:"Salvador",
+        uf: "BA",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        clientesId: 2,
+        endereco : "Rua Álvaro Barbosa",
+        bairro: "Jardim São Lucas",
+        numero:"468",
+        complemento:"CLIENTE PESSOA JURIDICA 1",
+        cidade: "Pirassununga",
+        uf:"SP",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }],
+    {})
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('endenrecos', null, {});
+  }
+};
